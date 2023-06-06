ec6ef812fd4ce6ce4b87608e59e273fa632a853a
nikogenix <nikogenix@gmail.com>
2023-06-06 06:01:52 +0300

    Attempting another deployment

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+#url="http://localhost:5000/health"
+url="https://rough-surf-7755.fly.dev/health"
 
-exit 0
+response=$(curl -s "$url")
+
+expected_value="ok"
+
+if [ "$response" = "$expected_value" ]; then
+    echo "0"
+    exit 0
+else
+    echo "1"
+    exit 1
+fi
